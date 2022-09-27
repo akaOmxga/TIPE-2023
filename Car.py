b932f1c6aeed6df8a76b800ea67b735fb5ac79c9
TeKo <emilien797@gmail.com>
2022-09-27 15:45:08 +0200

    Ajout fonction qui donne la voiture la plus proche

--- Car.py
+++ Car.py
@@ -4,6 +4,8 @@ class Car:
 
     mass = 1240 # kg
 
+
+    # Pour créer un objet voiture : ``car = Car(vitesse, x, z)`` exemple : ``maVoiture = new Car(15, 0, 0)`` 
     def __init__(self, speed, x, z):
         self.speed = speed # m.s^(-1)
         self.accel = 0
@@ -29,15 +31,42 @@ class Car:
         
         return(sqrt((abs(x2 - x1))**2 + abs(z2 - z1)**2))
 
+
+    # Renvoie la vitesse relative entre cet objet et la voiture passée en argument
     def getRelativeSpeed(self, otherCar):
         return(otherCar.speed - self.speed)
 
 
+    # Calcule la prochaine position de la voiture, la définit et la renvoie
     def calculateNextPosition(self, dt):
         
         # Ici, fait pour que ça fonctionne dans le cas unidimensionnel selon x seulement (on ne touche pas à z)
         # En gros : on considère que les vecteurs vitesse et accélération sont selon Ux seulement
         x, z = self.position
-        
+
         self.position = (x + self.speed*dt, z)
         return(self.position)
+
+
+    # Trouve la voiture la plus proche de l'objet dans une liste de voiture donnée (qui peut contenir l'objet lui même)
+    # Renvoie l'objet de la voiture la plus proche
+    # (Complexité : O(n))
+    def getNearestCar(self, carList):
+        if (len(carList) < 2):
+            return None
+        
+        if (carList[0] == self):
+            distanceMin = self.getDistanceBetween(carList[1])
+            closestCar = carList[1]
+        else:
+            distanceMin = self.getDistanceBetween(carList[0])
+            closestCar = carList[0]
+
+        for car in carList:
+            if (car != self):
+                distance = self.getDistanceBetween(car)
+                if (distance < distanceMin):
+                    distanceMin = distance
+                    closestCar = car
+        
+        return closestCar
