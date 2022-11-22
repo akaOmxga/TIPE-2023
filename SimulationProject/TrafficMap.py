d76f7013986fea3ffb12f85e480d08aae4beadbc
TeKo <emilien797@gmail.com>
2022-11-22 15:29:27 +0100

    Implémentation finalisée (a priori)

--- SimulationProject/TrafficMap.py
+++ SimulationProject/TrafficMap.py
@@ -1,5 +1,4 @@
 # L'objectif ici est d'avoir un visu sur toutes les voitures présentes sur chaque route en tout temps
-from Car import *
 
 class TrafficMap:
 
@@ -9,17 +8,12 @@ class TrafficMap:
     def __str__(self):
         return f"{self.traffic_map}"
 
-    # Créé une "route"
-    def addRoad(self, start_coords, end_coords):
-        if start_coords == end_coords:
-            raise Exception("Erreur : impossible de créer la route, elle n'est pas valide")
-
-        self.traffic_map[(start_coords, end_coords)] = []
-
     # Ajoute la voiture car sur le segment de route donné
     def addCarOnRoad(self, start_coords, end_coords, car):
+
+        # Si la route n'existe pas dans le dico, on la créée
         if (start_coords, end_coords) not in self.traffic_map:
-            raise Exception("Erreur : la route n'exist pas")
+            self.traffic_map[(start_coords, end_coords)] = []
         if start_coords == end_coords:
             raise Exception("Erreur : la route n'est pas valide")
 
@@ -29,41 +23,24 @@ class TrafficMap:
         self.traffic_map[(start_coords, end_coords)] = cars_on_road
 
     def deleteCarFromRoad(self, start_coords, end_coords, car):
+        # Si la route n'existe pas dans le dico, on la créée
+        if (start_coords, end_coords) not in self.traffic_map:
+            self.traffic_map[(start_coords, end_coords)] = []
+
         cars_on_road = self.traffic_map[(start_coords, end_coords)]
+        if (cars_on_road == []):
+            return
         if (car not in cars_on_road):
             raise Exception("Erreur : la voiture n'est pas sur la route")
 
+
+        # Normalement, la voiture qu'on retire se trouve toujours à l'indice 0, mais on traite le cas général dans le doute.
         index = cars_on_road.index(car)
         n = len(cars_on_road)
         if (index != n - 1):
-            for i in range (index, len(cars_on_road) - 2):
-                cars_on_road[index] = cars_on_road[index + 1]
+            for i in range (index, len(cars_on_road) - 1):
+                cars_on_road[i] = cars_on_road[i + 1]
 
         cars_on_road = cars_on_road[:-1]
 
         self.traffic_map[(start_coords, end_coords)] = cars_on_road
-
-
-# TODO : implémenter une file (les voitures seront rangées les unes à la suite des autres et pop quand elles
-# sortent de la route
-# Faire aussi en sorte de pouvoir intervertir 2 élt dans le futur (pour des dépassements)
-
-trafficMap = TrafficMap()
-
-voiture1 = Car(10, 1, 1, 1)
-voiture2 = Car(10, 10, 1, 1)
-voiture3 = Car(10, 20, 1, 1)
-voiture4 = Car(10, 30, 1, 1)
-
-trafficMap.addRoad((1, 1, 1), (50, 1, 1))
-
-trafficMap.addCarOnRoad((1, 1, 1), (50, 1, 1), voiture1)
-trafficMap.addCarOnRoad((1, 1, 1), (50, 1, 1), voiture2)
-trafficMap.addCarOnRoad((1, 1, 1), (50, 1, 1), voiture3)
-trafficMap.addCarOnRoad((1, 1, 1), (50, 1, 1), voiture4)
-
-print(trafficMap)
-
-voiture1.speed = 5
-
-print(trafficMap)
