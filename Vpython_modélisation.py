f7d7e5a0c93dc2e0cda212952e54203365da2983
akaOmxga <114494587+akaOmxga@users.noreply.github.com>
2023-01-03 14:43:43 +0100

    Update Vpython_modélisation.py

--- Vpython_modélisation.py
+++ Vpython_modélisation.py
@@ -54,6 +54,88 @@ class Car :
 y_reference  = 5
 largeur_reference = 15
 
+class NetworkGraph:
+
+    def __init__(self):
+        self.network = {} # Graphe (dictionnaire) sous la formé k,v : coordonnées, sommets connectés (liste d'adj)
+
+    def __str__(self):
+        return f"{self.network}"
+
+    # Créé un sommet correspondant physiquement à l'extrémité d'une route
+    # x, y, z sont les coordonnées de l'extrémité de la route
+    # Méthode privée car on ne doit a priori pas en rajouter un hors de cette classe
+    def __addSommet(self, coords):
+        self.network[coords] = []
+
+    # Ajoute une arête entre les sommets start et end (représentés par leurs coordonnées)
+    def addConnection(self, start, end, curved):
+        x, y, z = end
+        voisins_de_s1 = self.network[start] + [(x, y, z, curved)]
+        self.network[start] = voisins_de_s1
+
+    # Créé les arêtes du graphe.
+    # Physiquement, cela correspond à une route ou start et end sont les couples de coordonnées de début et fin de route
+    # Si un sommet (extrêmité de route n'existe pas -> on le créé avec addSommet)
+    # start et end sont de la formes (x1, y1, z1) et (x2, y2, z2), curved est un boolean (false = route droite)
+    def addEdge(self, start, end, curved = False):
+        if start == end:
+            raise Exception("Erreur : le départ et l'arrivée d'une route ne peuvent pas être confondus")
+        if start not in self.network:
+            self.__addSommet(start)
+        if end not in self.network:
+            self.__addSommet(end)
+
+        self.addConnection(start, end, curved)
+        
+# L'objectif ici est d'avoir un visu sur toutes les voitures présentes sur chaque route en tout temps
+
+class TrafficMap:
+
+    def __init__(self):
+        self.traffic_map = {} # Dictionnaire sous la formé k,v : (coordonnées début de route, coordonnées fin de route), liste des voitures sur cette route
+
+    def __str__(self):
+        return f"{self.traffic_map}"
+
+    # Ajoute la voiture car sur le segment de route donné
+    def addCarOnRoad(self, start_coords, end_coords, car):
+
+        # Si la route n'existe pas dans le dico, on la créée
+        if (start_coords, end_coords) not in self.traffic_map:
+            self.traffic_map[(start_coords, end_coords)] = []
+        if start_coords == end_coords:
+            raise Exception("Erreur : la route n'est pas valide")
+
+        cars_on_road = self.traffic_map[(start_coords, end_coords)]
+        cars_on_road += [car]
+
+        self.traffic_map[(start_coords, end_coords)] = cars_on_road
+
+    def deleteCarFromRoad(self, start_coords, end_coords, car):
+        # Si la route n'existe pas dans le dico, on la créée
+        if (start_coords, end_coords) not in self.traffic_map:
+            self.traffic_map[(start_coords, end_coords)] = []
+
+        cars_on_road = self.traffic_map[(start_coords, end_coords)]
+        if (cars_on_road == []):
+            return
+        if (car not in cars_on_road):
+            raise Exception("Erreur : la voiture n'est pas sur la route")
+
+
+        # Normalement, la voiture qu'on retire se trouve toujours à l'indice 0, mais on traite le cas général dans le doute.
+        index = cars_on_road.index(car)
+        n = len(cars_on_road)
+        if (index != n - 1):
+            for i in range (index, len(cars_on_road) - 1):
+                cars_on_road[i] = cars_on_road[i + 1]
+
+        cars_on_road = cars_on_road[:-1]
+
+        self.traffic_map[(start_coords, end_coords)] = cars_on_road
+        
+
 class Reseau :
     
     def __init__(self,start,end) : # start = (a,b,c) et end = (x,y,z) sont les coords de début et de fin de la route
