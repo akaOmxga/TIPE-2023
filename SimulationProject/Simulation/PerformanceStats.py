578f15eede1cdafd3a5c9b065ab3148bab3775c6
TeKo <emilien797@gmail.com>
2023-05-30 15:23:17 +0200

    fixes again

--- SimulationProject/Simulation/PerformanceStats.py
+++ SimulationProject/Simulation/PerformanceStats.py
@@ -24,13 +24,13 @@ class PerformanceStats:
         self.distance = 0 # somme des longueurs de trajets effectués
         self.temps_reel = [] # liste contenant le temps mis par chaque véhicule pour effectuer leur trajet
         self.temps_ideal = [] # liste contenant le temps minimal de chaque véhicule pour effectuer leur trajet
-        self.vitesse = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcourt
+        self.vitesse = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcours
       #  self.densite_max_autoroute = 0 # densité maximale du réseau d'autoroute pendant la simulation (il est envisageable de faire de même avec le min et les autres types de route), l'objectif de l'optimisation étant de minimiser (indirectement) cette densité
         ### envisageable ultérieurement (l'implémentation rencontrera probablement des problèmes de complexité mémoire ...)
-      #  self.vitesse_autoroute = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcourt sur autoroute
-      #  self.vitesse_nationale = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcourt sur nationale
-      #  self.vitesse_departementale = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcourt sur departementale
-      #  self.vitesse_ville = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcourt en ville
+      #  self.vitesse_autoroute = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcours sur autoroute
+      #  self.vitesse_nationale = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcours sur nationale
+      #  self.vitesse_departementale = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcours sur departementale
+      #  self.vitesse_ville = [] # liste contenant les vitesse moyennes de chaque véhicule pendant leur parcours en ville
       
     def __str__(self):
         return("Durée simu : " + str(self.simulation_object.internal_clock) +
@@ -41,43 +41,49 @@ class PerformanceStats:
             "\n\nTemps réels total : " + str(self.temps_reel) +
             "\n\nTemps idéaux total : " + str(self.temps_ideal) +
             "\n\nVitesses moyennes : " + str(self.vitesse) +
-            "\n\n\nTemps moyen de parcourt : " + str(self.temps_moy()) +
-            "\nRetard moyen de parcourt : " + str(self.retard_moy()) +
-            "\nVitesse moyenne de parcourt : " + str(self.vitesse_moy()) +
+            "\n\n\nTemps moyen de parcours : " + str(self.temps_moy()) +
+            "\nRetard moyen de parcours : " + str(self.retard_moy()) +
+            "\nVitesse moyenne de parcours : " + str(self.vitesse_moy()) +
             "\nFlux moyen : " + str(self.flux_moy()) +
             "\nIndice de congestion : " + str(self.indice_congestion())
             )
 
-    # temps moyen de parcourt (en secondes)
+    # temps moyen de parcours (en secondes)
     def temps_moy(self):
         temps_total = 0
-        for temps_individuel in self.temps_reel : 
-            temps_total += temps_individuel
-        return temps_total/self.voitures_apparues
 
-    # l'écart-type du temps de parcours / moyenne du retard (moyenne de la différence entre temps de parcourt réel et idéal) (en s)
+        for temps_individuel in self.temps_reel: 
+            temps_total += temps_individuel
+
+        return temps_total / self.voitures_arrivees
+
+    # l'écart-type du temps de parcours / moyenne du retard (moyenne de la différence entre temps de parcours réel et idéal) (en s)
     def retard_moy(self):
         retard_total = 0
         N = len(self.temps_reel)
-        for i in range(N) :
-            retard_total += (self.temps_reel[i] - self.temps_ideal[i])
-        return retard_total/self.voitures_apparues
 
-    # vitesse moyenne de parcourt (en m/s)
+        for i in range(N):
+            retard_total += (self.temps_reel[i] - self.temps_ideal[i])
+
+        return retard_total/self.voitures_arrivees
+
+    # vitesse moyenne de parcours (en m/s)
     def vitesse_moy(self):
         vitesse_total = 0
-        for vitesse in self.vitesse :
+        
+        for vitesse in self.vitesse:
             vitesse_total += vitesse
-        return vitesse_total/self.voitures_apparues
 
-    # flux moyen de parcourt (nombre de voiture/s)
+        return vitesse_total/self.voitures_arrivees
+
+    # flux moyen de parcours (nombre de voiture/s)
     def flux_moy(self):
         return self.voitures_apparues / self.simulation_object.internal_clock
 
     # pourcentage de temps passer en "congestion" avec pour norme : congestion ssi v < 70% limitation de vitesse
     def indice_congestion(self):
         temps_total = 0
-        for temps_individuel in self.temps_reel : 
+        for temps_individuel in self.temps_reel: 
             temps_total += temps_individuel
-        return self.congestion_time/temps_total
+        return self.congestion_time / temps_total
     
